ba25eb4b7000c7defa7e2114497039a5d06d66ec
Michael Dufour <mikerhinosfr@gmail.com>
2016-05-11 09:54:06 +0200

    Finished course1

--- Course1_/course1_/course1_.py
+++ Course1_/course1_/course1_.py
@@ -1,6 +1,34 @@
-# To change this license header, choose License Headers in Project Properties.
-# To change this template file, choose Tools | Templates
-# and open the template in the editor.
+from statistics import mean as m
+
+admins = {"Python":"Pass123@","user2":"pass2"}
+studentDict = { "Jeff":[78,88,93],
+                "Alex":[92,76,88],
+                "Sam":[89,92,93]}
+
+def enterGrades():
+    nameToEnter = input("Student name: ")
+    gradeToEnter = input("Grade: ")
+    
+    if nameToEnter in studentDict:
+        print("Adding grade...")
+        studentDict[nameToEnter].append(float(gradeToEnter))
+    else:
+        print("Student does not exist.")
+    print(studentDict)
+    
+def removeStudent():
+    nameToRemove = input("What student to remove? : ")
+    if nameToRemove in studentDict:
+        print("Removing student...")
+        del studentDict[nameToRemove]
+    print(studentDict)
+
+def studentAVGs():
+    for eachStudent in studentDict:
+        gradeList = studentDict[eachStudent]
+        avgGrade = m(gradeList)
+    print(eachStudent,"has an average grade of",avgGrade)
+    print(studentDict)
 
 def main():
     print("""
@@ -11,3 +39,28 @@ def main():
         [3] - Student Average Grades
         [4] - Exit
     """)
+    action = input("What would you like to do today? (Enter a number) ")
+    
+    if action == "1":
+        enterGrades()
+    elif action == "2":
+        removeStudent()
+    elif action == "3":
+        studentAVGs()
+    elif action == "4":
+        exit()
+    else :
+        print("No valid choice was given, please try again")
+        
+login = input("Username: ")
+passw = input("Password: ")
+
+if login in admins:
+    if admins[login] == passw:
+        print("Welcome", login)
+        while True:
+            main()
+    else: print("Invalid password, will detonate in 5 seconds")
+else: print("Invalid username, calling the FBI to report this")
+ 
+
