3fa08882c97e72a65ce9b66ad3caaf34ceeb9689
Michael Dufour <mikerhinosfr@gmail.com>
2016-05-11 08:28:09 +0200

    Ajout def main

---
+++ Course1_/course1_/course1_.py
@@ -0,0 +1,13 @@
+# To change this license header, choose License Headers in Project Properties.
+# To change this template file, choose Tools | Templates
+# and open the template in the editor.
+
+def main():
+    print("""
+        Welcome to Grade Central
+        
+        [1] - Enter Grades
+        [2] - Delete Student
+        [3] - Student Average Grades
+        [4] - Exit
+    """)
